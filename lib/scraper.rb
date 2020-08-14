34f4d4a47b138cfedc2ea4096d64cb0486e06c11
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-14 18:07:37 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,11 +8,19 @@ class Scraper
     doc = Nokogiri::HTML(html)
     student_cards = doc.css(".student-card a")
     student_cards.collect do |element|
-      
+      {:name => element.css(".student-name").text ,
+        :location => element.css(".student-location").text,
+        :profile_url => element.attr('href')}
+    end 
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    html = open(index_url)
+    doc = Nokogiri::HTML(html)
+    return_hash{}
+      social = doc.css("vitals-container.social-icon-container a")
+      social.each do |element| #assign a key to social elements if it exsists
+        
   end
 
 end
