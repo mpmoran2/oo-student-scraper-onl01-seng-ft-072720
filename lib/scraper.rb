9eba7f5509f9b29d7933301da6b0e06b01277ec5
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-14 18:08:17 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -30,7 +30,9 @@ class Scraper
           return_hash[:blog] = element.attr('href')
         end
       end
-      
+      return_hash[:profile_quote] = doc.css(".vitals-container .vitals-text-container .profile-quote").text
+      return_hash[:bio] = doc.css(".bio-block.details-block .bio-content.content-holder .description-holder p").text
+      return_hash
   end
 
 end
